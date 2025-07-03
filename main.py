af8eb37e6b1eb6f6c868b398992a6ecbe56beb4a
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-07-03 18:34:26 -0500

    adding handling for dns scan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ipaddress
 
-from scapy.all import DNS, DNSQR, IP, TCP, UDP, sr
+from scapy.all import DNS, DNSQR, ICMP, IP, TCP, UDP, sr, sr1
 
 # imports everything (func, classes, vars) from scapy.all without prefixing them with 'scapy.all.<func>' but can litter namespace and can cause issues if 2 things have the same name. similar to usenamespace std;
 # from scapy.all import *
@@ -8,6 +8,7 @@ from scapy.all import DNS, DNSQR, IP, TCP, UDP, sr
 
 class Setup:
     __common_ports = (22, 25, 80, 443, 445, 8080, 8443)
+    SOURCE_PORT = 3333
 
     def __init__(self, host):
         self.host = host
@@ -20,16 +21,19 @@ class Setup:
         print(f"Scanning (SYN) {self.host}")
         """
         - all other flags will be set by scapy automatically
+            - Manually is <protocol/layer>(<flags> = ..)
         - returns a tuple full of 'scapy QueryAnswer' objects, of answered results and unanswered results. answered does not mean port is opened, 
             it just simply saying that we got a reponse back from the request we sent
         - 'IP / TCP' builds the SYN packet and 'sr' (send and recieve) sends them to the host and ports
         - Timeout of 2 sec, saying if it doesn't hear back from target after request send in 2 sec it will close it
         - 'sr' sends packets at layer 3 and waits for a reply 
-            - it will send it and the OS checks its routing table: if the IP is on the local subnet
+            - it will send it and the OS checks its routing table: if the IP is on the local subnet -> do ARP for device, else ARP for defualt gateway then route to it
+            - OS wraps the IP packet in an Ethernet frame with the proper MAC and sends the full Ethernet frame via NIC.
         """
         answered, unanswered = sr(
-            IP(dst=self.host) /
-            TCP(sport=9999, dport=list(Setup.__common_ports), flags="S"),
+            IP(dst=self.host) / TCP(sport=Setup.SOURCE_PORT,
+                                    dport=list(Setup.__common_ports),
+                                    flags="S"),
             timeout=2,
             verbose=0,
         )
@@ -39,32 +43,57 @@ class Setup:
         - go through the packet we sent and received from the answered tuple. Packet is of IP type
         - accessing the TCP layer of each packet
         """
-
         for sent, recv in answered:
             if sent[TCP].dport == recv[TCP].sport and recv[TCP].flags == "SA":
                 print(f"{recv[TCP].sport} open")
 
     def dns_scan(self):
         """
-        sends DNS request (port 53) to host and sees if it responds with DNS reponse, thus telling us it's a DNS server
+        - sends DNS request (port 53) to host and sees if it responds with DNS reponse, thus telling us it's a DNS server
+        - If port is closed, it will send out an ICMP "port unreachable"
         """
 
         print(f"Scanning (DNS) {self.host}")
 
-        # crafts packet and sends it
+        # crafts IP (3) -> UDP (4) -> DNS (5/7) packet and sends it
         packet = (
-            IP(dst=self.host) / UDP(dport=53) /
-            # 'rd' is
+            IP(dst=self.host) / UDP(sport=Setup.SOURCE_PORT, dport=53) /
+            # 'rd' (recursion desired flag), tells DNS server "if you don't know the answer go find it for me", common for client -> DNS requests
+            # 'qd' is the query data flag, with a DNS Query Record, with domain name google.com. By defualt it will have 'qtype' is A
             DNS(rd=1, qd=DNSQR(qname="google.com")))
 
-        answered, unanswered = sr(packet, timeout=2, verbose=0)
-
-        if answered and answered[UDP]:
-            print(f"{self.host} is a DNS Server")
+        response = sr1(packet, timeout=2, verbose=0)
+        # answered, _ = sr(packet, timeout=1, verbose=0)
+        """checks if UDP data is inside the response, not very accurate as it can still give false positive if the server sends back any UDP data. It needs to check if its a DNS response aswell"""
+        # if response and UDP in response:
+        #     print(f"{self.host} is a DNS Server")
+        # else:
+        #     print(f"No DNS server found on {self.host}")
+        """
+        so this is better:
+        - Unlike TCP where we recieve a RST (reset) = true packet, in UDP if the request does not go through the dest will send a ICMP Type 3 Code 3 (Port Unreachable)
+            response, unless filitered
+        """
+        print(response)
+        if response:  # makes sure we actually get a response, we will recieve someting if it fails or goes through
+            if response.haslayer(DNS):
+                dns_layer = response[DNS]
+                if dns_layer.qr == 1 and dns_layer.rcode == 0:
+                    print(f"{self.host} is a DNS Server")
+                else:
+                    if response.haslayer(ICMP):
+                        icmp_layer = response[ICMP]
+                        if icmp_layer.type == 3 and icmp_layer.code == 3:
+                            print(
+                                f"No DNS server found on {self.host} (port unreachable)"
+                            )
+            else:
+                print(f"No DNS server found on {self.host}")
         else:
             print(f"No DNS server found on {self.host}")
 
 
-x = Setup("8.8.8.8")
+# x = Setup("8.8.8.8")
+x = Setup("127.0.0.1")
 x.syn_scan()
 x.dns_scan()
